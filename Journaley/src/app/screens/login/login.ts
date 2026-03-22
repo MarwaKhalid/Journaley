f6e0726c4db9a4ecc9ec0028a7d5110c83ce9a0b
MarwaKhalid <marwaa.khaliid@gmail.com>
2026-03-22 09:16:16 -0400

    Completed login screen

--- Journaley/src/app/screens/login/login.ts
+++ Journaley/src/app/screens/login/login.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { TextButton } from '../../components/buttons/text-button/text-button';
+import { InputField } from '../../components/input-field/input-field';
 
 @Component({
   selector: 'app-login',
-  imports: [TextButton],
+  imports: [TextButton, InputField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email: string = '';
+  password: string = '';
+
+  onClick() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+}
