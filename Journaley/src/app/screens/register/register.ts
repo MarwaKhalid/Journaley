9eaa631f65d26a9358236fcb58facb04b011377c
MarwaKhalid <marwaa.khaliid@gmail.com>
2026-03-22 09:16:28 -0400

    Completed register screen

--- Journaley/src/app/screens/register/register.ts
+++ Journaley/src/app/screens/register/register.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { TextButton } from '../../components/buttons/text-button/text-button';
+import { InputField } from '../../components/input-field/input-field';
 
 @Component({
   selector: 'app-register',
-  imports: [TextButton],
+  imports: [TextButton, InputField],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  confirm_password: string = '';
+
+  onClick() {
+    console.log('Name:', this.name);
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Confirm Password:', this.confirm_password);
+  }
+}
