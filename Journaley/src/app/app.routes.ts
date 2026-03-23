7d267245a643a36757a6e4d1aa35f30bf582a9e2
MarwaKhalid <marwaa.khaliid@gmail.com>
2026-03-23 15:23:16 -0400

    Implemented Home temporarily and fixed routing

--- Journaley/src/app/app.routes.ts
+++ Journaley/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './screens/home/home';
+import { Login } from './screens/login/login';
+import { Register } from './screens/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+];
